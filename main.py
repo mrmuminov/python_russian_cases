94272429e66343cd95a29f481a4181f70a23a41b
bahriddin <darkshadeuz@gmail.com>
2025-11-27 17:42:18 +0500

    Add markdowner Disable CORS

--- main.py
+++ main.py
@@ -3,29 +3,41 @@ import time
 from typing import Dict, Union
 
 from fastapi import FastAPI, HTTPException, Depends, Query
-from fastapi.responses import PlainTextResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from markdown2 import Markdown
 
 from config import CASE_TAGS
 from services import WordInflector, get_word_inflector
 
 app = FastAPI()
+markdowner = Markdown()
+
+# Add CORS middleware to allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
-@app.get("/", response_class=PlainTextResponse)
+@app.get("/", response_class=HTMLResponse)
 def read_root() -> str:
     """Returns the content of the README.md file."""
     try:
         with open("README.md", "r", encoding="utf-8") as f:
-            return f.read()
+            return markdowner.convert(f.read())
     except FileNotFoundError:
-        return "Welcome to the API."
+        return "<h1>Welcome to the API</h1>"
 
 
 @app.get("/{case}")
 def inflect_word(
-    case: str,
-    q: str = Query(..., description="The word to inflect."),
-    inflector: WordInflector = Depends(get_word_inflector),
+        case: str,
+        q: str = Query(..., description="The word to inflect."),
+        inflector: WordInflector = Depends(get_word_inflector),
 ) -> Dict[str, Union[str, float, Dict[str, str]]]:
     """
     Inflects a word to a given case or all cases.
